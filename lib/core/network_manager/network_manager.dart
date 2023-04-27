bc547d5080e410dbcc3501a43917b733c65690d9
Taha Furkan Cansizoğlu <tahacansizoglu@gmail.com>
2023-04-27 10:22:04 +0300

    final

--- lib/core/network_manager/network_manager.dart
+++ lib/core/network_manager/network_manager.dart
@@ -4,12 +4,10 @@ import 'package:workshop_example/product/api_endpoints/api_endpoints.dart';
 class NetworkManager {
   late final Dio _dio = Dio(BaseOptions(baseUrl: ApiEndpoints.baseUrl));
 
-  @override
   Future<Response> get(String path, {Map<String, dynamic>? queryParameters}) {
     return _dio.get(path, queryParameters: queryParameters);
   }
 
-  @override
   Future<Response> post(String path, {Map<String, dynamic>? queryParameters, data}) {
     try {
       return _dio.post(path, queryParameters: queryParameters, data: data);
